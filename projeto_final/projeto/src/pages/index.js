73c39a5a1a5a6a44e0e856a1b8ba3bc0930a178d
Bruno Borges <bruninho-borgesf@hotmail.com>
2022-05-24 22:16:51 -0300

    Aula 24/05/2022 - projeto final da aula

--- projeto_final/projeto/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className={`bg-gradient-to-r from-green-500 to-blue-500 h-screen`}>
-      <h1>
-        Pagina Admin
-      </h1>
-    </div>
-  )
-}
---
+++ projeto_final/projeto/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <Layout  titulo="Pagina Inicial" subtitulo="Estamos construindo um tamplate admin">
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint amet sunt quasi a maiores exercitationem, eaque doloremque odio cupiditate, iste quidem. Nobis exercitationem officia veniam adipisci dolor iure rem natus? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam quasi suscipit sequi minima autem minus veritatis mollitia officiis, non obcaecati tempore facilis maxime magni molestiae recusandae aperiam sint sit architecto.
+      </p>
+    </Layout>
+
+  )
+}
